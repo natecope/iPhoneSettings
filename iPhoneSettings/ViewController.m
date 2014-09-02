4fec80057cc22af9dc416d896209a23f04432e9e
Nathan Cope <natecope@gmail.com>
2014-09-02 18:07:49 -0400

    - Save/load settings using NSUserDefaults

--- iPhoneSettings/ViewController.m
+++ iPhoneSettings/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *sizeTextField;
+@property (weak, nonatomic) IBOutlet UISwitch *videoSwitch;
+@property (weak, nonatomic) IBOutlet UISlider *volumeSlider;
+
 @end
 
 @implementation ViewController
@@ -18,12 +22,56 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // register settings with nsuserdefaults
+    [self registerDefaultSettings];
+    
+    // load settings
+    [self loadSettings];
+
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void)registerDefaultSettings{
+    
+    NSDictionary *settings = @{@"fontSize": @10,
+                               @"videoOn": @NO,
+                               @"volumeSliderMin": @0,
+                               @"volumeSliderMax": @100,
+                               @"volume" : @15
+                               };
+    
+    
+    [[NSUserDefaults standardUserDefaults]registerDefaults:settings];
+    
 }
 
+-(void)loadSettings {
+    self.sizeTextField.text = [[NSUserDefaults standardUserDefaults]stringForKey:@"fontSize"];
+    self.videoSwitch.on = [[NSUserDefaults standardUserDefaults] boolForKey:@"videoOn"];
+    self.volumeSlider.minimumValue = [[NSUserDefaults standardUserDefaults] floatForKey:@"volumeSliderMin"];
+    self.volumeSlider.maximumValue = [[NSUserDefaults standardUserDefaults] floatForKey:@"volumeSliderMax"];
+    self.volumeSlider.value = [[NSUserDefaults standardUserDefaults] floatForKey:@"volume"];
+}
+
+-(void)saveSettings{
+    
+    
+    [[NSUserDefaults standardUserDefaults]setObject:self.sizeTextField.text forKey:@"fontSize"];
+    [[NSUserDefaults standardUserDefaults]setBool:self.videoSwitch.on forKey:@"videoOn"];
+    [[NSUserDefaults standardUserDefaults]setFloat:self.volumeSlider.minimumValue forKey:@"volumeSliderMin"];
+    [[NSUserDefaults standardUserDefaults]setFloat:self.volumeSlider.maximumValue forKey:@"volumeSliderMax"];
+    [[NSUserDefaults standardUserDefaults]setFloat:self.volumeSlider.value forKey:@"volume"];
+    
+    
+    [[NSUserDefaults standardUserDefaults]synchronize];
+}
+
+- (IBAction)doneButtonPressed:(id)sender {
+    
+    
+    [self saveSettings];
+    
+}
 @end
